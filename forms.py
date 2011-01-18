c643e9daa744bca085d1ada9e241e2d777ee8989
Ted <ted.tieken@gmail.com>
2011-01-17 20:31:39 -0800

    Added note.

--- forms.py
+++ forms.py
@@ -2,6 +2,8 @@ from django import forms
 from privatebeta.models import InviteRequest
 
 class InviteRequestForm(forms.ModelForm):
+    note = forms.CharField(label="Note (optional)", widget=forms.Textarea(attrs={'class':'note'}))
+    
     class Meta:
         model = InviteRequest
-        fields = ['email']
+        fields = ['email', 'note']
--- models.py
+++ models.py
@@ -5,6 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 class InviteRequest(models.Model):
     email = models.EmailField(_('Email address'), unique=True)
     created = models.DateTimeField(_('Created'), default=datetime.datetime.now)
+    note = models.TextField(_('Note'), max_length=5000, blank=True, null=True)
     invited = models.BooleanField(_('Invited'), default=False)
 
     def __unicode__(self):
